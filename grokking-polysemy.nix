98f66a58eb3e0ca5cf1b855c0009af5cd2be832b
Yuan Wang <me@yuanwang.ca>
2020-04-07 08:26:14 -0600

    update nixpkgs version

--- grokking-polysemy.nix
+++ grokking-polysemy.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, polysemy, stdenv }:
+{ mkDerivation, base, polysemy, polysemy-plugin, stdenv }:
 mkDerivation {
   pname = "grokking-polysemy";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base polysemy ];
+  executableHaskellDepends = [ base polysemy polysemy-plugin ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
