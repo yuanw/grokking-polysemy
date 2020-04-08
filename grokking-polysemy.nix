760bd208086e52f145b423c269fc44099cfe9ba4
Yuan Wang <me@yuanwang.ca>
2020-04-08 13:10:14 -0600

    fix the build

--- grokking-polysemy.nix
+++ grokking-polysemy.nix
@@ -1,11 +1,16 @@
-{ mkDerivation, base, polysemy, polysemy-plugin, stdenv }:
+{ mkDerivation, base, polysemy, polysemy-plugin, random, stdenv }:
 mkDerivation {
   pname = "grokking-polysemy";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base polysemy polysemy-plugin ];
+  executableHaskellDepends = [
+    base
+    polysemy
+    polysemy-plugin
+    random
+  ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
